912f765b1e9ecfd7acda201542fe9ed9a2c68047
WuJiLong <1105105330@nkust.edu.tw>
2020-05-19 23:21:43 +0800

    初始專案 Hello, world!

---
+++ Minecraft_Plugin/src/wuchilong/mc/plugin/main.java
@@ -0,0 +1,14 @@
+package wuchilong.mc.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin{
+	public void onEnable() {
+		getLogger().info("正在開啟...");
+		
+	}
+	public void onDisable() {
+		getLogger().info("正在關閉...");
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Minecraft_Plugin
+Minecraft版本：1.15
+自我練習用專案
